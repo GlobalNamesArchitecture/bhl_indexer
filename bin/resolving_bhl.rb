b2e236bbb44c84f74b376610307c99de9382cc5a
Dmitry Mozzherin <dmozzherin@gmail.com>
2012-08-24 16:25:54 -0400

    updated name resolution

--- bin/resolving_bhl.rb
+++ bin/resolving_bhl.rb
@@ -5,22 +5,15 @@
 
 require_relative '../environment.rb'
 
-a = 'huh?'
-q = "\nDo you REALLY want to truncate %s resolved name strings? y/n" % ResolvedNameString.count.to_s
-
-until ['y','n','yes','no','yeah','nah'].include?(a)
-  puts q
-  a = gets.strip
-end
-
-if ['yes','y','yeah'].include?(a)
+if ARGV[0] == '--truncate'
+  puts "Truncating data"
   Title.connection.execute("truncate table resolved_canonical_forms")
   Title.connection.execute("truncate table resolved_name_strings")
   Title.connection.execute("update name_strings set status = 0")
 end
 
 resolver = BHLIndexer::ResolverClient.new
-resolver.rebuild_resolved_names_hash
+# resolver.rebuild_resolved_names_hash
 
 rows_num = 1
 until rows_num == 0
